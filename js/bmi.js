d75468b2d30e1d6d8234b4e6754204303238548e
Mubashshir-Dev <mubashshir.tech@gmail.com>
2026-07-16 23:50:43 +0530

    bmi js

---
+++ js/bmi.js
@@ -0,0 +1,7 @@
+var weight = prompt("weight in kg : ");
+var height = prompt("height in m^2 : ");
+
+var bmi = weight/(height)*(height);
+
+console.log("bmi is : "+bmi+" kg/m^2");
+alert("bmi is : "+bmi+" kg/m^2");
